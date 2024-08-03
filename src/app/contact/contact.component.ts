0bb4828353bec33e3b35dc821f326107c740eaad
NikharPanchal <Panchalnikhar4920@gmail.com>
2024-08-03 21:58:12 +0530

    Add new content

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,12 +1,39 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatFormFieldModule, MatLabel } from '@angular/material/form-field';
+import { MatIcon, MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [],
+  imports: [MatFormFieldModule, MatLabel, MatInputModule, MatIconModule, MatIcon, ReactiveFormsModule, FormsModule,CommonModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.css'
 })
-export class ContactComponent {
+export class ContactComponent implements OnInit {
+  registerform!: FormGroup;
+  subimitted: boolean = false;
+  returnurl!: string;
+  message = "";
+  emailmessage = "";
+  private userArr: any = [];
 
+  constructor(private formbuilder: FormBuilder) {
+
+    this.registerform = this.formbuilder.group({
+      // id: this.formbuilder.control(['', Validators.compose([Validators.required, Validators.minLength(5)])]),
+      fname: ['', Validators.required],
+      lname: ['', Validators.required],
+      email: ['', Validators.compose([Validators.required, Validators.email])],
+      phonenumber : ['',Validators.required],
+      note: ['', Validators.compose([Validators.required])]
+    })
+  }
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+
+  register() {}
 }
