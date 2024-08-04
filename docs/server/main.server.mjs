80e8b6f444d715cf45fe2c5e2090f514243e929b
NikharPanchal <Panchalnikhar4920@gmail.com>
2024-08-04 10:39:24 +0530

    fixed responsice navbar issue

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-YVOLZ6DE.mjs";import"./chunk-VQ6B5KAS.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-C3XJGJFA.mjs";import"./chunk-VQ6B5KAS.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
